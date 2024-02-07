6b40f647630dc772d9641c5a03176e00816843fa
camenduru <54370274+camenduru@users.noreply.github.com>
2024-02-07 03:33:11 +0300

    test

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import os
 # os.system('cp -r ./_ckpt/LLaVA-7B-v1 /data/LLaVA-7B-v1'), os.system('cp -r ./_ckpt/mgie_7b /data/mgie_7b')
 os.system('ls /data'), os.system('df -h /data')
-[os.system('mv llava.py /home/user/.pyenv/versions/3.10.13/lib/python3.10/site-packages/llava/model/llava.py'), 
- os.system('mv train.py /home/user/.pyenv/versions/3.10.13/lib/python3.10/site-packages/llava/train/train.py')]
+[os.system('mv llava.py /usr/local/lib/python3.10/dist-packages/llava/model/llava.py'), 
+ os.system('mv train.py /usr/local/lib/python3.10/dist-packages/llava/train/train.py')]
 
 from PIL import Image
 
